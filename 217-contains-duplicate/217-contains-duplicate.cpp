b4bf28300f4fc98e9fd620fc064b19c419c3d296
Deepanshu Varun <86783410+ShankyDV@users.noreply.github.com>
2022-08-01 23:17:05 +0530

    Time: 210 ms (11.09%), Space: 53.6 MB (15.71%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -3,7 +3,7 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_set<int> s;
         bool flag = false;
-        for(auto it: nums){
+        for(auto &it: nums){
             if(s.find(it)!=s.end()){
                 flag = true;
             }
